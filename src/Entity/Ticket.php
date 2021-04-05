43d3fca8d4b5e7a1f5d91531c08838780970599b
Ivan <doctor254452009@ya.ru>
2021-04-05 09:25:26 +0500

    add airport and ticket entity

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TicketRepository::class)
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Airport::class, inversedBy="tickets")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $departureAirport;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $departureTime;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Airport::class, inversedBy="tickets")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $arrivalAirport;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $arrivalTime;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDepartureAirport(): ?Airport
+    {
+        return $this->departureAirport;
+    }
+
+    public function setDepartureAirport(?Airport $departureAirport): self
+    {
+        $this->departureAirport = $departureAirport;
+
+        return $this;
+    }
+
+    public function getDepartureTime(): ?\DateTimeInterface
+    {
+        return $this->departureTime;
+    }
+
+    public function setDepartureTime(\DateTimeInterface $departureTime): self
+    {
+        $this->departureTime = $departureTime;
+
+        return $this;
+    }
+
+    public function getArrivalAirport(): ?Airport
+    {
+        return $this->arrivalAirport;
+    }
+
+    public function setArrivalAirport(?Airport $arrivalAirport): self
+    {
+        $this->arrivalAirport = $arrivalAirport;
+
+        return $this;
+    }
+
+    public function getArrivalTime(): ?\DateTimeInterface
+    {
+        return $this->arrivalTime;
+    }
+
+    public function setArrivalTime(\DateTimeInterface $arrivalTime): self
+    {
+        $this->arrivalTime = $arrivalTime;
+
+        return $this;
+    }
+}
